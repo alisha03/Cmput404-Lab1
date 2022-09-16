1a9550a7243e27cadfc83e9512120a20890cb4d0
alisha03 <crasta@ualberta.ca>
2022-09-16 15:10:15 -0600

    updated script

--- lab1.py
+++ lab1.py
@@ -2,3 +2,7 @@ import requests
 print(requests.__version__)
 
 r = requests.get("http://www.google.com")
+print(r.text)
+
+r = requests.get("https://raw.githubusercontent.com/alisha03/Cmput404-Lab1/master/lab1.py")
+print(r.text)
